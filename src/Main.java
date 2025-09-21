fc70bbcb39b542d293f0625347683dd9295143f1
Hoang Quan <messlionel05@gmail.com>
2025-09-21 21:58:18 +0700

    Done progject

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,19 @@
 //MagicBuffDecorator (+10 power).
 //
 //Viết chương trình: tạo Warrior có sword + armor + buff.
+public class Main {
+    public static void Play() {
+        Character warrior = new Warrior();
+        Character mage = new Mage();
+        warrior = new Armor(warrior);
+        warrior = new Sword(warrior);
 
-public class Main{
+        mage = new Magic(mage);
+        System.out.println(warrior.getDescription() + " with power: " + warrior.getPower());
+        System.out.println(mage.getDescription() + " with power: " + mage.getPower());
+    }
+
     public static void main(String[] args) {
-        
+        Play();
     }
 }
